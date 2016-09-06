292530aa4d93655a75bc265f6e0b72e3f6536730
valery <developman36@gmail.com>
2016-09-06 13:33:59 +0300

    fix rules

--- config/rules.php
+++ config/rules.php
@@ -1,11 +1,6 @@
 <?php
 return [
     '/' => 'site/index',
-    '/about' => 'site/about',
-    '/contact' => 'site/contact',
-    '/entry' => 'site/entry',
-    '/country' => 'country/index',
-    '/login' => 'site/login',
-    '' => '',
-    '' => '',
+    '/wagon' => 'wagon/index',
+    '/type-wagon' => 'type-wagon/index',
 ];
